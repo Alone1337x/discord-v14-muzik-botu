b572988bba930d1118368a1018131f48f79b6b53
Alone1337x <148453920+Alone1337x@users.noreply.github.com>
2024-07-29 23:34:24 +0300

    Update ayarlar.js
    Müzik bot

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "TOKEN GİR",  // bot token gir :)
+    TOKEN: process.env.TOKEN || "MTI2NzU3MTY0MTY0NzYyODQwMg.Gj_FPx.33Tv_3EQG_wB_o0pQ-qifGV2bWave_ni25EW6U",  // bot token gir :)
     PREFIX: process.env.PREFIX || "!", // bot prefix
-    SAHİP: process.env.SAHİP || "SAHİP İD", //sahip id :()
+    SAHİP: process.env.SAHİP || "1115945869427085372", //sahip id :()
 }
